23b2745a745046894dd1b0574c72c28203f191b9
Hector Robles <hjt.robles@gmail.com>
2020-06-06 23:11:42 +0800

    Updates

---
+++ precache-manifest.20719735f63e74039b5b6585bfa2af74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44ccc467f98b4b901220b4921decdee4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5a5dd13fb5e1f5eeb377",
+    "url": "/portfolio/static/js/2.f175dcd4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio/static/js/2.f175dcd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1774b7ad7d0c0425844",
+    "url": "/portfolio/static/js/main.6379daf1.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
