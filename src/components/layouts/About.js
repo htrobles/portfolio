26ffdc5b65549e75d20b27cafb80b05cbcf51a84
Hector Robles <hjt.robles@gmail.com>
2020-06-18 03:35:35 +0800

    update about section

--- src/components/layouts/About.js
+++ src/components/layouts/About.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
 	return (
@@ -7,36 +7,77 @@ const About = () => {
 				<div className="container py-5">
 					<div className="row p-lg-5">
 						<div className="col-12 mb-4">
-							<h5>ABOUT HECTOR</h5>
-							<hr/>
-							<p className="about__text mb-4">Being a designer first, Hector Robles demonstrates his creativity through CSS maneuvering and an atmosphere of combined minimalistic approach and vibrancy. His clever use of colors are creatively put together to showcase elements of simplicity and visual consistency. Kicking it up a notch, his artistic psyche delved deeper into web developing as this was the key to his art to not just be beautiful, but also interactive and functional. As he incorporates this career into his portfolio, he sees the future of websites becoming the perfect canvass for his art.</p>
+							<h5>ABOUT ME</h5>
+							<hr />
 						</div>
-						<div className="col-md-4 mb-5 mb-md-0">
-							<img src="./assets/images/artist.svg" alt="Hector Robles" className="about__img img-fluid px-5 mb-3 mb-lg-5"/>
-							<div className="copy">
-								<h5 className="text-center">Artist</h5>
-								<p className="text-center">Having graduated with a Fine Arts degree, he has acquired a set of skills that made him a multifaceted designer. Hector's family owns a publishing company that has given him many opportunities to practice his craft as a designer at a very young age. </p>
+						<div className="col-md-12 mb-5">
+							<div className="row mb-4">
+								<div className="mx-auto about__img-container d-md-flex align-items-md-center col-md-4 mb-4 mb-md-0">
+									<img
+										src="./assets/images/artist.svg"
+										alt="Hector Robles"
+										className="about__img img-fluid px-5 mb-3 mb-md-0"
+									/>
+								</div>
+								<div className="copy col-md-8 d-md-flex flex-md-column justify-content-md-center">
+									<h5 className="text-center text-md-left">Designer</h5>
+									<p className="text-justify text-md-left">
+										I am a developer who is a designer first. I believe that
+										websites and web apps should not only be functional but also
+										pleasing to look at. My approach is UI/UX-driven. For me, to
+										make sure that the user gets results with ease and comfort
+										is as important as getting the correct results.
+									</p>
+								</div>
 							</div>
 						</div>
-						<div className="col-md-4 mb-5 mb-md-0">
-							<img src="./assets/images/developer.svg" alt="Hector Robles" className="about__img img-fluid px-5 mb-3 mb-lg-5"/>
-							<div className="copy">
-								<h5 className="text-center">Developer</h5>
-								<p className="text-center">Hector's interest in programming started when he was in grade school through Starcraft's map editor. Growing up he has aspired to be a game developer and is now applying his skills through self-study and incorporating it with his design skills through web development.</p>
+						<div className="col-md-12 mb-5">
+							<div className="row">
+								<div className="mx-auto about__img-container d-md-flex align-items-md-center col-md-4 mb-4 mb-md-0">
+									<img
+										src="./assets/images/developer.svg"
+										alt="Hector Robles"
+										className="about__img img-fluid px-5 mb-3 mb-md-0"
+									/>
+								</div>
+								<div className="copy col-md-8 d-md-flex flex-md-column justify-content-md-center">
+									<h5 className="text-center text-md-left">Developer</h5>
+									<p className="text-justify text-md-left">
+										I code because I want my designs to have life. Printed
+										brochures, magazines, or posters will never be interactive
+										unlike websites or apps. Coding gives me the perfect canvas
+										for my art. The possibilities of creating interactive
+										designs in coding are endless. I enjoy all aspects of web
+										development—from planning to deployment. I love art and I
+										love to code.
+									</p>
+								</div>
 							</div>
 						</div>
-						<div className="col-md-4 mb-5 mb-md-0">
-							<img src="./assets/images/cat.svg" alt="Hector Robles" className="about__img img-fluid px-5 mb-3 mb-lg-5"/>
-							<div className="copy">
-								<h5 className="text-center">Cat Lover</h5>
-								<p className="text-center">A self-proclaimed ailurophile. Two cats jumping on his workspace has become a common occurence, but it only says so much about how he focuses on work despite the cute distractions around him.</p>
+						<div className="col-md-12 mb-5">
+							<div className="row">
+								<div className="mx-auto about__img-container d-md-flex align-items-md-center col-md-4 mb-4 mb-md-0">
+									<img
+										src="./assets/images/cat.svg"
+										alt="Hector Robles"
+										className="about__img img-fluid px-5 mb-3 mb-md-0"
+									/>
+								</div>
+								<div className="copy col-md-8 d-md-flex flex-md-column justify-content-md-center">
+									<h5 className="text-center text-md-left">Cat Lover</h5>
+									<p className="text-justify text-md-left">
+										I love cats! Two cats jumping on my workspace has become a
+										common occurrence, but it only says so much about how I
+										focus on work despite the cute distractions around me.
+									</p>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	)
-}
+	);
+};
 
 export default About;
