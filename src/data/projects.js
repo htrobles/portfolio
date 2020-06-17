fb0fabe52928cae59270076711cc91f47da867cd
Hector Robles <hjt.robles@gmail.com>
2020-06-18 03:54:33 +0800

    temporarily remove wrkst8ions, added burger-builder project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,43 +1,78 @@
 module.exports = [
 	{
-		id	: "1",
-		title : "The Red Brick Oven",
-		description : "A static website for a pizza restaurant.",
-		image: "red-brick-oven.jpg",
-		url: "https://robleshector.gitlab.io/the-red-brick-oven",
-		technologies : ["HTML","CSS", "SaSS", "Bootstrap"]
+		id: '1',
+		title: 'The Red Brick Oven',
+		description: 'A static website for a pizza restaurant.',
+		image: 'red-brick-oven.jpg',
+		url: 'https://robleshector.gitlab.io/the-red-brick-oven',
+		technologies: ['HTML', 'CSS', 'SaSS', 'Bootstrap'],
 	},
 	{
-		id	: "2",
-		title : "Fleet Management System",
-		description : "A fleet management web app for handling company vehicles",
-		image: "fms.jpg",
-		url: "http://fms-fleet-management-system.herokuapp.com/",
-		technologies : ["PHP", "Laravel", "MySQL", "Composer", "HTML","CSS", "SaSS", "Bootstrap"]
+		id: '2',
+		title: 'Fleet Management System',
+		description: 'A fleet management web app for handling company vehicles',
+		image: 'fms.jpg',
+		url: 'http://fms-fleet-management-system.herokuapp.com/',
+		technologies: [
+			'PHP',
+			'Laravel',
+			'MySQL',
+			'Composer',
+			'HTML',
+			'CSS',
+			'SaSS',
+			'Bootstrap',
+		],
+	},
+	// {
+	// 	id	: "3",
+	// 	title : "Wrkst8ions",
+	// 	description : "A workspace booking website with admin and back-end server",
+	// 	image: "wrkst8ions.jpg",
+	// 	url: "https://wrkst8ions-client.herokuapp.com/",
+	// 	technologies : ["MongoDB", "ExpressJS", "ReactJS", "Node.js", "HTML","CSS", "SaSS", "Bootstrap"]
+	// },
+	{
+		id: '3',
+		title: 'Burger Builder',
+		description:
+			'A React web app that allows a user to build a burger with set ingredients',
+		image: 'burger-builder.jpg',
+		url: 'https://react-my-burger-b1bf3.web.app/',
+		technologies: [
+			'ReactJS',
+			'Redux',
+			'Node.js',
+			'Firebase',
+			'HTML',
+			'CSS',
+			'SaSS',
+			'Bootstrap',
+		],
 	},
 	{
-		id	: "3",
-		title : "Wrkst8ions",
-		description : "A workspace booking website with admin and back-end server",
-		image: "wrkst8ions.jpg",
-		url: "https://wrkst8ions-client.herokuapp.com/",
-		technologies : ["MongoDB", "ExpressJS", "ReactJS", "Node.js", "HTML","CSS", "SaSS", "Bootstrap"]
+		id: '4',
+		title: 'Inkwell Publishing Co., Inc.',
+		description:
+			'Static website for a small publishing company in the Philippines',
+		image: 'inkwell.jpg',
+		url: 'https://inkwellphilippines.com/',
+		technologies: ['HTML', 'CSS', 'SaSS'],
 	},
 	{
-		id	: "4",
-		title : "Inkwell Publishing Co., Inc.",
-		description : "Static website for a small publishing company in the Philippines",
-		image: "inkwell.jpg",
-		url: "https://inkwellphilippines.com/",
-		technologies : ["HTML","CSS", "SaSS"]
+		id: '5',
+		title: 'Yelpcamp',
+		description:
+			'A campground website where users can post campgrounds and post comments',
+		image: 'yelpcamp.jpg',
+		url: 'https://yelpcamp-by-hector.herokuapp.com/',
+		technologies: [
+			'MongoDB',
+			'ExpressJS',
+			'Node.js',
+			'HTML',
+			'CSS',
+			'Bootstrap',
+		],
 	},
-	{
-		id	: "5",
-		title : "Yelpcamp",
-		description : "A campground website where users can post campgrounds and post comments",
-		image: "yelpcamp.jpg",
-		url: "https://yelpcamp-by-hector.herokuapp.com/",
-		technologies : ["MongoDB", "ExpressJS", "Node.js", "HTML","CSS", "Bootstrap"]
-	}
-]
-
+];
